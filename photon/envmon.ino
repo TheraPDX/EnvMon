d320b5a34b1fb4475279ff4ec2a862ab4b4f0747
Victor Jolissaint <vjolissaint@twitter.com>
2015-12-11 22:10:57 +0000

    feat: updated the loop logic and added a distance sensor

--- photon/envmon.ino
+++ photon/envmon.ino
@@ -1,32 +1,23 @@
-int light = 0;
-float temp = 0;
-char light_str[10];
-char temp_str[10];
 int led = D7;
-
+long LIGHT_TEMP_PUBLISH_INTERVAL = 2000;
+long DIST_PUBLISH_INTERVAL = 1000;
+unsigned long lastPublishLightTemp, lastPublishDist = 0;
 
 void setup() {
-    pinMode(led, OUTPUT);                 // http://docs.spark.io/firmware/#setup-pinmode
-    Spark.variable("light", &light, INT); // http://docs.spark.io/firmware/#spark-variable
-    pinMode(A0, INPUT);                   // LDR is linked to this analog pin
-    pinMode(A1, INPUT);                   // LM35 is linked to this analog pin
+    pinMode(led, OUTPUT);
 }
 
 void loop() {
-    light = analogRead(A0);
-    sprintf(light_str, "%i", light);
-    Spark.publish("getLight", light_str, PRIVATE);
+    if(millis() - lastPublishLightTemp >= LIGHT_TEMP_PUBLISH_INTERVAL) {
+        lastPublishLightTemp = millis();
+        publishLight(A0);
+        publishTemp(A1);
+    }
 
-    int sensorValue = analogRead(A1);
-    temp = convertTempReading(sensorValue);
-    sprintf(temp_str, "%.1f", temp);
-    Spark.publish("getTemp", temp_str, PRIVATE);
-
-    // for easy debugging, we turn on / off the LED with each publish
-    digitalWrite(led, HIGH);
-    delay(200);
-    digitalWrite(led, LOW);
-    delay(1800);
+    if(millis() - lastPublishDist >= DIST_PUBLISH_INTERVAL) {
+        lastPublishDist = millis();
+        ping(D0, D1, 20);
+    }
 }
 
 float convertTempReading(int sensorValue) {
@@ -35,7 +26,70 @@ float convertTempReading(int sensorValue) {
 
     // Converts to Kelvin
     float kelvinValue = (voltage * 100);
-    //float celsiusValue = kelvinValue - 273.15;
 
     return kelvinValue;
+}
+
+void publishLight(pin_t light_pin) {
+    static bool init = false;
+    if (!init) {
+        pinMode(light_pin, INPUT);
+        init = true;
+    }
+
+    int sensorValue = analogRead(light_pin);
+
+    char light_str[10];
+    sprintf(light_str, "%i", sensorValue);
+
+    Spark.publish("getLight", light_str, PRIVATE);
+}
+
+void publishTemp(pin_t temp_pin) {
+    static bool init = false;
+    if (!init) {
+        pinMode(temp_pin, INPUT);
+        init = true;
+    }
+
+    int sensorValue = analogRead(temp_pin);
+    float temp = convertTempReading(sensorValue);
+
+    char temp_str[10];
+    sprintf(temp_str, "%.1f", temp);
+
+    Spark.publish("getTemp", temp_str, PRIVATE);
+}
+
+/*
+  Adapted from https://gist.github.com/technobly/349a916fb2cdeb372b5e
+*/
+void ping(pin_t trig_pin, pin_t echo_pin, uint32_t wait) {
+    uint32_t duration, cm;
+    static bool init = false;
+    if (!init) {
+        pinMode(trig_pin, OUTPUT);
+        digitalWriteFast(trig_pin, LOW);
+        pinMode(echo_pin, INPUT);
+        delay(50);
+        init = true;
+    }
+
+    /* Trigger the sensor by sending a HIGH pulse of 10 or more microseconds */
+    digitalWriteFast(trig_pin, HIGH);
+    delayMicroseconds(10);
+    digitalWriteFast(trig_pin, LOW);
+
+    duration = pulseIn(echo_pin, HIGH);
+
+    /* Convert the time into a distance */
+    // Sound travels at 1130 ft/s (73.746 us/inch)
+    // or 340 m/s (29 us/cm), out and back so divide by 2
+    // Ref: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+    cm = duration / 29 / 2;
+
+    char dist_str[10];
+    sprintf(dist_str, "%i", cm);
+
+    Spark.publish("getDist", dist_str, PRIVATE);
 }
